94bbab20b8bbd795bb0d8be858d2c6df4a2c4fbc
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2025-02-19 18:09:13 +0200

    Simplify test

--- test/retro_counter_test.exs
+++ test/retro_counter_test.exs
@@ -2,7 +2,6 @@ defmodule RetroCounterTest do
   use ExUnit.Case
 
   test "smoke" do
-    pid = RetroCounter.start(:normal, [])
-    assert pid != nil
+    assert RetroCounter.start(:normal, []) != nil
   end
 end
