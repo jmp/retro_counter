8cba4e18ade3e3fac3c6dec8c270aee75eaff943
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2025-02-22 16:56:23 +0200

    Read path from env variables

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,7 +1,11 @@
 import Config
 
+config :retro_counter,
+  count_path: System.get_env("RETRO_COUNTER_PATH", "count.txt")
+
 if config_env() == :test do
   {:ok, _} = Application.ensure_all_started(:briefly)
 
-  config :retro_counter, count_path: Briefly.create!()
+  config :retro_counter,
+    count_path: Briefly.create!()
 end
