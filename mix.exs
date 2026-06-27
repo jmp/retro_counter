138f08a96dbbdd9be78c594869e3a5bbe8c57985
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2026-06-27 12:22:54 +0300

    Upgrade to Elixir 1.20

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule RetroCounter.MixProject do
     [
       app: :retro_counter,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.20",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
